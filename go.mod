ef200c43e6b38887269f561273f9d2a76c89ef1e
Jhon Franco <jhonfranco424@gmail.com>
2023-04-14 22:58:09 -0500

    Algoritmos de factorizacion y recursion izquierda, falta probar

--- go.mod
+++ go.mod
@@ -2,7 +2,28 @@ module github.com/JhonF424/LL1
 
 go 1.20
 
+require gioui.org v0.0.0-20230414223051-b6e0376ad2fe
+
 require (
-	golang.org/x/tools v0.7.0
-	gorm.io/gorm v1.24.6
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.6 // indirect
+	github.com/go-text/typesetting v0.0.0-20230405155246-bf9c697c6e16 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/therecipe/env_darwin_amd64_513 v0.0.0-20190626001412-d8e92e8db4d0 // indirect
+	github.com/therecipe/env_linux_amd64_513 v0.0.0-20190626000307-e137a3934da6 // indirect
+	github.com/therecipe/env_windows_amd64_513 v0.0.0-20190626000028-79ec8bd06fb2 // indirect
+	github.com/therecipe/env_windows_amd64_513/Tools v0.0.0-20190626000028-79ec8bd06fb2 // indirect
+	github.com/therecipe/qt v0.0.0-20200904063919-c0c124a5770d // indirect
+	github.com/therecipe/qt/internal/binding/files/docs/5.12.0 v0.0.0-20200904063919-c0c124a5770d // indirect
+	github.com/therecipe/qt/internal/binding/files/docs/5.13.0 v0.0.0-20200904063919-c0c124a5770d // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
+	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
+	golang.org/x/image v0.5.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/JhonF424/LL1/controllers"
@@ -34,9 +36,9 @@ func main() {
 	}*/
 
 	/*gramatica := []models.Grammar{
-		"S": {"a", "B", "C", "d"},
-		"B": {"C", "B", "b"},
-		"C": {"c", "c", "lambda"},
+		{Symbol: "S", Productions: []string{"a", "B", "C", "d"}},
+		{Symbol: "B", Productions: []string{"C", "B", "b"}},
+		{Symbol: "C", Productions: []string{"c", "c", "lambda"}},
 	}*/
 
 	gramatica := []models.Grammar{
@@ -77,52 +79,135 @@ func main() {
 		}
 		fmt.Print("}")
 	}
-
 }
 
-/*
-func resolverRecursionIzquierda(gramatica map[string]string) map[string]string {
-	nuevoNoTerminal := "X"                          // Nuevo símbolo no terminal para representar la recursión izquierda
-	produccionesNuevas := make(map[string][]string) // Mapa para almacenar las nuevas producciones
-
-	// Iterar sobre cada producción de la gramática
-	for nt, prod := range gramatica {
-		producciones := strings.Split(prod, "|")
-		produccionesNuevas[nt] = make([]string, 0)
-		recursiones := make([]string, 0)
-
-		// Revisar si hay una recursión izquierda en la producción
-		for _, p := range producciones {
-			if strings.HasPrefix(p, nt) { // Si hay una recursión izquierda
-				recursiones = append(recursiones, p)
-			} else {
-				produccionesNuevas[nt] = append(produccionesNuevas[nt], p)
-			}
-		}
-
-		// Si hay recursiones izquierdas, generar nuevas producciones
-		if len(recursiones) > 0 {
-			nuevoNT := nt + nuevoNoTerminal // Nuevo símbolo no terminal para la recursión
-			produccionesNuevas[nuevoNT] = make([]string, 0)
-
-			for _, p := range producciones {
-				if !strings.HasPrefix(p, nt) { // Si no hay recursión izquierda
-					produccionesNuevas[nt] = append(produccionesNuevas[nt], p+nuevoNT)
-				} else { // Si hay recursión izquierda
-					produccionesNuevas[nuevoNT] = append(produccionesNuevas[nuevoNT], strings.TrimPrefix(p, nt)+nuevoNT)
+func Factorizar(grammar []models.Grammar) []models.Grammar {
+	for i := 0; i < len(grammar); i++ {
+		for j := 0; j < len(grammar[i].Productions); j++ {
+			for k := j + 1; k < len(grammar[i].Productions); k++ {
+				if grammar[i].Productions[j][0] == grammar[i].Productions[k][0] {
+					temp := commonPrefix(grammar[i].Productions[j], grammar[i].Productions[k])
+					if temp != "" {
+						nonTerminal := fmt.Sprintf("%s'", temp)
+						grammar = addGrammar(grammar, nonTerminal, []string{temp + string(nonTerminal[1])})
+						grammar[i].Productions[j] = strings.Replace(grammar[i].Productions[j], temp, nonTerminal, 1)
+						grammar[i].Productions[k] = strings.Replace(grammar[i].Productions[k], temp, nonTerminal, 1)
+						grammar = removeGrammar(grammar, models.Grammar{Symbol: grammar[i].Symbol, Productions: []string{}})
+						i = 0
+						break
+					}
 				}
 			}
+		}
+	}
+	return grammar
+}
 
-			// Agregar producción epsilon al nuevo símbolo no terminal
-			produccionesNuevas[nuevoNT] = append(produccionesNuevas[nuevoNT], "epsilon")
+// EliminarRecursionIzquierda elimina la recursión izquierda de una gramática
+func EliminarRecursionIzquierda(grammar []models.Grammar) []models.Grammar {
+	for i := range grammar {
+		symbol := grammar[i].Symbol
+		productions := grammar[i].Productions
+		newProductions := make([]string, 0)
+		nonTerminals := make([]string, 0)
+
+		// separar producciones recursivas y no recursivas
+		for _, production := range productions {
+			if strings.HasPrefix(production, symbol) {
+				nonTerminals = append(nonTerminals, production)
+			} else {
+				newProductions = append(newProductions, production)
+			}
+		}
+
+		if len(nonTerminals) == 0 {
+			continue
+		}
+
+		// construir nuevos símbolos no terminales
+		newSymbols := make([]string, len(nonTerminals))
+		for j := range nonTerminals {
+			newSymbol := symbol + "'" + strconv.Itoa(j)
+			newSymbols[j] = newSymbol
+		}
+
+		// crear nuevas producciones sin recursión
+		for _, production := range newProductions {
+			grammar = addGrammar(grammar, symbol, []string{production})
+		}
+
+		// crear nuevas producciones con los nuevos símbolos no terminales
+		for j, nonTerminal := range nonTerminals {
+			productions := strings.Split(nonTerminal[len(symbol):], "|")
+			newProductions := make([]string, len(productions))
+			for k, production := range productions {
+				if strings.HasPrefix(production, symbol) {
+					production = strings.Replace(production, symbol, newSymbols[j], 1)
+				}
+				newProductions[k] = production + newSymbols[j]
+			}
+			grammar = addGrammar(grammar, newSymbols[j], newProductions)
+		}
+
+		// crear producciones con los nuevos símbolos no terminales para las producciones restantes
+		for _, nonTerminal := range nonTerminals {
+			productions := strings.Split(nonTerminal[len(symbol):], "|")
+			newProductions := make([]string, len(productions))
+			for k, production := range productions {
+				newProductions[k] = production + newSymbols[0]
+			}
+			grammar = addGrammar(grammar, symbol, newProductions)
+		}
+
+		// eliminar producción original
+		grammar = removeGrammar(grammar, symbol, productions)
+	}
+
+	return grammar
+}
+
+// Agrega una producción a la gramática
+func addGrammar(grammar []models.Grammar, symbol string, production []string) []models.Grammar {
+	for i := range grammar {
+		if grammar[i].Symbol == symbol {
+			grammar[i].Productions = append(grammar[i].Productions, production...)
+			return grammar
 		}
 	}
 
-	// Actualizar la gramática con las nuevas producciones
-	for nt, prod := range produccionesNuevas {
-		gramatica[nt] = strings.Join(prod, "|")
+	grammar = append(grammar, models.Grammar{Symbol: symbol, Productions: production})
+	return grammar
+}
+
+// Remueve una producción de la gramática
+func removeGrammar(grammar []models.Grammar, symbol string, production []string) []models.Grammar {
+	for i := range grammar {
+		if grammar[i].Symbol == symbol {
+			grammar[i].Productions = remove(grammar[i].Productions, production)
+			return grammar
+		}
 	}
 
-	return gramatica
+	return grammar
 }
-*/
+
+// Encuentra el prefijo común más largo entre dos strings
+func commonPrefix(s1, s2 string) string {
+	var prefix string
+	for i := 0; i < len(s1) && i < len(s2); i++ {
+		if s1[i] != s2[i] {
+			break
+		}
+		prefix += string(s1[i])
+	}
+	return prefix
+}
+
+func remove(slice []string, s []string) []string {
+	for i, v := range slice {
+		if reflect.DeepEqual(v, s) {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
+}
